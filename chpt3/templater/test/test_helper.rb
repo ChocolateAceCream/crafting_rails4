e8bb78d0b83902277b12c45ee9ebee744a79b7a1
ChocolateAceCream <ubuntu@ip-172-31-62-146.ec2.internal>
2017-02-13 15:42:38 +0000

    finished chpt3

--- chpt3/templater/test/test_helper.rb
+++ chpt3/templater/test/test_helper.rb
@@ -2,7 +2,17 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require "capybara"
+require "capybara/rails"
+
+# Define a bare test case to use with Capybara
+class ActiveSupport::IntegrationsCase < ActiveSupport::TestCase
+    include Capybara::DSL
+    include Rails.application.routes.url_helpers
+end
+
 class ActiveSupport::TestCase
+    ActiveRecord::Migration.check_pending!
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
